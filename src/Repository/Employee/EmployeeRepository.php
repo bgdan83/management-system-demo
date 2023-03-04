69b2f7e804af3a783abaef6c49a9e7df86e521fb
bgdan83 <maklev@mail.ru>
2023-03-04 23:56:12 +0300

    Перенес работу с базой в repository

--- src/Repository/Employee/EmployeeRepository.php
+++ src/Repository/Employee/EmployeeRepository.php
@@ -5,14 +5,38 @@ namespace App\Repository\Employee;
 use App\Entity\Employee;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class EmployeeRepository extends ServiceEntityRepository
 {
     
+    /**
+     * @var EntityManagerInterface
+     */
+    protected EntityManagerInterface $entityManager;
+    
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Employee::class);
+        
+        $this->entityManager = $this->getEntityManager();
+    }
+    
+    public function add(Employee $employee) 
+    {
+        $this->entityManager->persist($employee);
+        $this->entityManager->flush();
+    }
+    
+    public function update(Employee $employee) 
+    {
+        $this->entityManager->flush();
     }
 
+    public function delete(Employee $employee) 
+    {
+        $this->entityManager->remove($employee);
+        $this->entityManager->flush();
+    }
 }

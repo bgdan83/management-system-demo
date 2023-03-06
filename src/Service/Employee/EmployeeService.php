7d09f6b49c787aee4947afebcc8e114dd5f9d510
bgdan83 <maklev@mail.ru>
2023-03-06 18:52:07 +0300

    Добавил комментарии

--- src/Service/Employee/EmployeeService.php
+++ src/Service/Employee/EmployeeService.php
@@ -4,15 +4,43 @@ namespace App\Service\Employee;
 use App\Entity\Employee;
 use Symfony\Component\HttpFoundation\Request;
 
-
+/** 
+ * Интерфейс работы с сотрудниками
+ */
 interface EmployeeService {
     
+    
+    /** 
+     *  Метод возвращает всех сотрудников
+     *  
+     *  @return
+     */
     function getAllEmployees();
     
+    /** 
+     *  Метод добавляет сотрудника
+     * 
+     *  @param Request $request
+     *  @param $form
+     *  @param Employee $employee
+     *  @return bool
+     */
     function addEmployeeForm(Request $request, $form, Employee $employee);
     
+    /** 
+     *  Метод обновляет данные сотрудника
+     * 
+     *  @param Request $request
+     *  @param $form
+     *  @param Employee $employee
+     *  @return bool
+     */
     function updateEmployeeForm(Request $request, $form, Employee $employee);
     
+    /** 
+     *  Метод удаляет сотрудника
+     * 
+     */
     function deleteEmployeeForm(Employee $employee);
 
 }
--- src/Service/Employee/Impl/EmployeeServiceImpl.php
+++ src/Service/Employee/Impl/EmployeeServiceImpl.php
@@ -7,6 +7,9 @@ use App\Service\Employee\EmployeeService;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Employee;
 
+/** 
+ *  Class EmployeeServiceImpl  
+ */
 class EmployeeServiceImpl implements EmployeeService
 {
 
@@ -23,46 +26,73 @@ class EmployeeServiceImpl implements EmployeeService
     {
         $this->employeeRepository = $employeeRepository;
     }
-
+    
+    /** 
+     *  Метод возвращает всех сотрудников
+     *  
+     *  @return
+     */
     public function getAllEmployees()
     {
         return $this->employeeRepository->findAll();
     }
-
+    
+    /** 
+     *  Метод добавляет сотрудника
+     * 
+     *  @param Request $request
+     *  @param $form
+     *  @param Employee $employee
+     *  @return bool
+     */
     public function addEmployeeForm( 
         Request $request,
         $form,
         Employee $employee
     ): bool {
         
+        $result = false;
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->employeeRepository->add($employee);
    
-            return true;
+            $result = true;
         }
         
-        return false;
+        return $result;
     }
     
+    /** 
+     *  Метод обновляет данные сотрудника
+     * 
+     *  @param Request $request
+     *  @param $form
+     *  @param Employee $employee
+     *  @return bool
+     */
     public function updateEmployeeForm( 
         Request $request,
         $form,
         Employee $employee
     ): bool {
         
+        $result = false;
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->employeeRepository->update($employee);
    
-            return true;
+            $result = true;
         }
         
-        return false;
+        return $result;
     }
     
+    /** 
+     *  Метод удаляет сотрудника
+     * 
+     */
     public function deleteEmployeeForm(Employee $employee) 
     {
         $this->employeeRepository->delete($employee);
